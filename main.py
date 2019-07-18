7ddbcaa62a21f6fa51bfbac382bf14299b642736
lrazzzorl <52835620+lrazzzorl@users.noreply.github.com>
2019-07-18 16:42:16 +0300

    начало 2

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import random as rnd
 
 
 class Game:
+
    def __init__(self, width=5, height=5, num_mines=3):
+       self.width = width
+       self.height = height
        self.board = [[0 for j in range(height)]for i in range(width)]
        # установка мин
        for i in range(num_mines):
-           self.board[rnd.randrange(width, height)] = 'X'
+           self.board[rnd.randrange(0, width)][rnd.randrange(0, height)] = 'X'
 
-
+   def paint(self):
+        for i in range(self.width):
+            print(i, end=' ')
 
 
 
@@ -20,3 +25,4 @@ class Game:
 
 
 my_game = Game()
+my_game.paint()
