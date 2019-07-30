14d39eef700e0c241156b2b766bcf7c43aee6bad
lrazzzorl <52835620+lrazzzorl@users.noreply.github.com>
2019-07-30 20:29:40 +0300

    coding...

--- main.py
+++ main.py
@@ -33,16 +33,41 @@ class Game:
 
 
     def paint(self):
+        # рисуем доску
         for i in range(self.height):
             for j in range(self.width):
                 # если клетки нет в открытых
-                if (i, j) in self.opened:
+                if (i, j) not in self.opened:
                     print('[ ]', end=' ')
                 else:
                     print(' {} '.format(self.board[i][j]), end=' ')
             # перевод строки
             print('')
 
+    def open(self, x, y):
+        # открываем ячейку
+        if self.board[x][y] == 'X':
+            print('Бомба!')
+            self.GameOver = True
+        else:
+            # составляем список из ячеек для открытия. если ячейцка 0 (мин рядом нет нет) - открываем её
+            To_Open = [(x, y)]
+            while To_Open:
+                i, j = To_Open.pop()
+                self.opened.append((i, j))
+                # соседи
+                if i > 0:
+                    if self.board[i - 1][j] != 'X' and (i - 1, j) not in self.opened:
+                        To_Open.append((i - 1, j))
+                if i < self.height - 1:
+                    if self.board[i + 1][j] != 'X' and (i + 1, j) not in self.opened:
+                        To_Open.append((i + 1, j))
+                if j > 0:
+                    if self.board[i][j - 1] != 'X' and (i, j - 1) not in self.opened:
+                        To_Open.append((i, j - 1))
+                if j < self.width - 1:
+                    if self.board[i][j + 1] != 'X' and (i, j + 1) not in self.opened:
+                        To_Open.append((i, j + 1))
 
     def play(self):
         # цикл продолжается пока не конец игры
@@ -53,6 +78,18 @@ class Game:
             inp = inp.lower()  # к нижнему регистру
             if inp == 'e':
                 self.GameOver = True
+            else:
+                #  пробуем считать цифры
+                try:
+                    inp = inp.split(',')
+                    x = int(inp[0])
+                    y = int(inp[1])
+                    if (0 <= x <= self.height - 1) and (0 <= y <= self.width - 1):
+                        self.open(x, y)
+                    else:
+                        print('Неверные данные!')
+                except ValueError or IndexError:
+                    print('Ошибка!')
 
 
 
@@ -63,5 +100,8 @@ class Game:
 
 
 
-my_game = Game(10, 10, 6)
+
+
+
+my_game = Game(10, 10, 20)
 my_game.play()
